ecc62a43588677c43827c184f5296bb55265dfdb
David010419 <dcastillo@global-retail.com>
2023-05-03 21:16:44 +0200

    03-05-2023

--- src/com/hedima/presentacion/Main.java
+++ src/com/hedima/presentacion/Main.java
@@ -1,5 +1,18 @@
+package com.hedima.presentacion;
+
+import com.hedima.persistencia.Conexion;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conexion c1 = new Conexion();
+        try {
+            c1.abrirConexion();
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
     }
 }
